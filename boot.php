857537ab8c3341e6e029d51cc9a225d9de9e1662
tyrant88 <webmaster@lautundschoen.de>
2016-08-11 12:36:55 +0200

    boot.php update

--- boot.php
+++ boot.php
@@ -6,17 +6,4 @@
  * Time: 10:24
  */
 
-    if (rex::isBackend()) {
-        /*rex_view::addCssFile($this->getAssetsUrl('redactor.css'));
-        rex_view::addCssFile($this->getAssetsUrl('redactor_custom.css'));
-        if (file_exists($this->getAssetsPath('skin.css'))) {
-            rex_view::addCssFile($this->getAssetsUrl('skin.css'));
-        }
-        rex_view::addJsFile($this->getAssetsUrl('redactor.js'));
-
-        $redactorLanguage = rex::getProperty('lang');
-        $redactorLanguage = substr($redactorLanguage, 0, 2);
-
-        rex_view::addJsFile($this->getAssetsUrl('langs/' . $redactorLanguage . '.js'));*/
-    }
 ?>
